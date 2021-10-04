c0f781869335564f7d793810c5fff822b03e082a
Avishek-kr <ak251997@gmail.com>
2021-10-04 23:50:41 +0530

    router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
+import Home from "./pages/Home";
 import React from "react";
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App;
+
